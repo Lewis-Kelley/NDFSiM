07fc5add53b1bae614c0efd594ae0e49ec8583ca
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-03-24 17:46:41 -0400

    Added command-arg checking and usage printout.

--- Main.hs
+++ Main.hs
@@ -6,6 +6,27 @@ import RunFSM
 
 main :: IO()
 main = do
-  (filename : string : _) <- getArgs
+  args <- getArgs
+  parseArgs args
+
+parseArgs :: [String] -> IO()
+parseArgs [] = printUsage
+parseArgs [filename] = runOnString filename ""
+parseArgs (filename : string : _) = runOnString filename string
+
+runOnString :: String -> String -> IO()
+runOnString filename string = do
   fsm <- loadFSM filename
   print $ runFSM fsm $ map (\ char -> pack [char]) string
+
+printUsage :: IO()
+printUsage =
+  let
+    message =
+      "Usage:\n" ++
+      "\t./Main <filename> (<string>)\n" ++
+      "Parameters:\n" ++
+      "\tfilename: the name of the .fsm file to load and run\n" ++
+      "\tstring: the string to be evaluated by the machine\n"
+  in
+    putStr message
