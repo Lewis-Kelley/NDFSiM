f22d8da95d0c6c4dd2bee396d2c50fa8a019c60a
Lewis-Kelley <kelleyld@rose-hulman.edu>
2018-04-21 10:04:59 -0400

    Added support for Regexes from the main CLI. Fixed some runner bugs.

--- Main.hs
+++ Main.hs
@@ -3,6 +3,8 @@ import System.Environment
 import FSM
 import ReadFSM
 import RunFSM
+import Regex
+import RegexToFSM
 
 main :: IO()
 main = do
@@ -12,7 +14,23 @@ main = do
 parseArgs :: [String] -> IO()
 parseArgs [] = printUsage
 parseArgs [filename] = runFSMOnString filename ""
+parseArgs ("-r" : regex : []) = runRegexOnString regex ""
+parseArgs ("-r" : regex : string : _) = runRegexOnString regex string
 parseArgs (filename : string : _) = runFSMOnString filename string
+
+printUsage :: IO()
+printUsage =
+  let
+    message =
+      "Usage:\n" ++
+      "\t./Main <filename> (<string>)\n" ++
+      "\t./Main -r <regex> (<string>)\n" ++
+      "Parameters:\n" ++
+      "\tfilename: the name of the .fsm file to load and run\n" ++
+      "\tregex: a regex to run\n" ++
+      "\tstring: the string to be evaluated by the machine\n"
+  in
+    putStr message
 
 runFSMOnString :: String -> String -> IO()
 runFSMOnString filename string = do
@@ -21,16 +39,11 @@ runFSMOnString filename string = do
     Nothing ->
       print ("Invalid FSM specified in " ++ filename)
     Just actFSM ->
-      print $ runFSM actFSM $ map Sym string
+      print $ runFSM actFSM $ map FSM.Sym string
 
-printUsage :: IO()
-printUsage =
-  let
-    message =
-      "Usage:\n" ++
-      "\t./Main <filename> (<string>)\n" ++
-      "Parameters:\n" ++
-      "\tfilename: the name of the .fsm file to load and run\n" ++
-      "\tstring: the string to be evaluated by the machine\n"
-  in
-    putStr message
+runRegexOnString :: String -> String -> IO()
+runRegexOnString regexStr string = do
+  case stringToRegex regexStr of
+    Nothing -> print ("Invalid regex specified with " ++ regexStr)
+    Just regex ->
+      print $ runFSM (regexToFSM regex) $ map FSM.Sym string
--- RunFSM.hs
+++ RunFSM.hs
@@ -4,19 +4,32 @@ import Data.Map.Strict as M (findWithDefault)
 import Data.Set as S
 import FSM
 
+type Configuration = (State, [Symbol])
+
 runFSM :: FSM -> [Symbol] -> Bool
 runFSM fsm@(_, _, _, start, _) string =
-  runStep fsm string start
+  runStep fsm (start, string) empty
 
-runStep :: FSM -> [Symbol] -> State -> Bool
-runStep (_, _, _, _, accepting) [] state = elem state accepting
-runStep fsm@(_, _, transitionMap, _, _) (next : rest) state =
-  let
-    children = findWithDefault empty (state, next) transitionMap
-    epsChildren = S.toList $ epsilonClosure fsm $ S.singleton state
-  in
-    any (\ child -> runStep fsm rest child) children
-    || any (\ epsChild -> runStep fsm rest epsChild) epsChildren
+runStep :: FSM -> Configuration -> Set Configuration -> Bool
+runStep fsm@(_, _, _, _, accepting) conf@(state, []) history
+  | elem conf history = False
+  | otherwise =
+    let
+      epsChildren = S.toList $ epsilonClosure fsm $ S.singleton state
+      newHistory = union history $ singleton conf
+    in
+      elem state accepting
+      || any (\ epsChild -> runStep fsm (epsChild, []) newHistory) epsChildren
+runStep fsm@(_, _, transitionMap, _, _) conf@(state, full@(next : rest)) history
+  | elem conf history = False
+  | otherwise =
+    let
+      children = findWithDefault empty (state, next) transitionMap
+      epsChildren = S.toList $ epsilonClosure fsm $ S.singleton state
+      newHistory = union history $ singleton conf
+    in
+      any (\ child -> runStep fsm (child, rest) newHistory) children
+      || any (\ epsChild -> runStep fsm (epsChild, full) newHistory) epsChildren
 
 epsilonClosure :: FSM -> Set State -> Set State
 epsilonClosure fsm@(_, _, transitionMap, _, _) states =
@@ -24,7 +37,7 @@ epsilonClosure fsm@(_, _, transitionMap, _, _) states =
     nested = S.map (\ state ->
                        findWithDefault empty (state, Epsilon) transitionMap) states
     currClosure = S.foldr (\ inner rest ->
-                              S.union inner rest) empty nested
+                              S.union inner rest) states nested
   in
     if currClosure == states
     then
